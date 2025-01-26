7b450a608f4e2ce6cbd78a5319e2487b7156fbe0
salomey tackie <salomeytackie555@gmail.com>
2025-01-26 18:21:34 +0000

    committing all changes

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const addButton = document.getElementById('add-task-btn');
+    const taskInput = document.getElementById('task-input');
+    const taskList = document.getElementById('task-list');
+    function addTask () {
+        const taskText = taskInput.value.trim();
+        if (taskText === '') {
+    alert ('Enter a task');
+}
+const newTask = document.createElement('li');
+  newTask.textContent = 'taskText';
+  const removebutton = document.createElement('button');
+  removebutton.textContent = 'remove';
+  removebutton.className = 'remove-btn';
+  taskList.appendChild(newTask);
+newTask.appendChild(removebutton);
+removebutton.onclick = function() {
+    taskList.removeChild(newtask);
+};
+      taskInput.value = ''; 
+}
+    addButton.addEventListener('click', addTask);
+    taskInput.addEventListener('keypress', (event) => {
+        if (event.key === 'Enter') {
+addTask();
+        }
+    });
+
+});
